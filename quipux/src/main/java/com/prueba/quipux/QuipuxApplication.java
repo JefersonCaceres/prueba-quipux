a7b300f79e6a4ecd2a3eb365299861cb5d22ba60
THOMASGREG\jefferson.caceres <jefferson.caceres@thomasgreg.com>
2026-01-20 21:55:18 -0500

    inicio de proyecto

---
+++ quipux/src/main/java/com/prueba/quipux/QuipuxApplication.java
@@ -0,0 +1,11 @@
+package com.prueba.quipux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipuxApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(QuipuxApplication.class, args);
+	}
+}
